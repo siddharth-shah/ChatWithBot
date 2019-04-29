cdafbf28f9233a49cfef2fd0155c1a668b2b6f2b
Siddharth <siddharth.shah@elanic.in>
2019-04-29 15:16:37 +0530

    Built new OkhttpClient which intercepts requests to add apiKey and chatBotId to each

--- app/src/main/java/co/chatbot/data/network/ApiClient.java
+++ app/src/main/java/co/chatbot/data/network/ApiClient.java
@@ -1,6 +1,13 @@
 package co.chatbot.data.network;
 
+import java.io.IOException;
+
+import co.chatbot.AppConstants;
+import okhttp3.HttpUrl;
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.moshi.MoshiConverterFactory;
@@ -18,8 +25,30 @@ public class ApiClient {
                     .baseUrl(BASE_URL)
                     .addConverterFactory(MoshiConverterFactory.create())
                     .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .client(getOkHTTPClient())
                     .build();
         }
         return retrofit;
+    }
+
+    private static OkHttpClient getOkHTTPClient() {
+        return new OkHttpClient.Builder()
+                .addInterceptor(new Interceptor() {
+                    @Override
+                    public Response intercept(Chain chain) throws IOException {
+                        Request request = chain.request();
+                        final HttpUrl url = request.url();
+                        final HttpUrl newUrl = url.newBuilder()
+                                .addQueryParameter(AppConstants.QUERY_PARAM_API_KEY, AppConstants.API_KEY)
+                                .addQueryParameter(AppConstants.QUERY_PARAM_CHAT_BOT_ID,
+                                        AppConstants.CHAT_BOT_ID).build();
+
+                        Request newRequest = request.newBuilder()
+                                .url(newUrl).build();
+                        return chain.proceed(newRequest);
+                    }
+                }).build();
+
+
     }
 }
