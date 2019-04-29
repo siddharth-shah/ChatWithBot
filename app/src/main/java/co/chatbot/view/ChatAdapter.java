6e18d4dcf1c094bffd488e5b604e04a5306082a9
Siddharth <siddharth.shah@elanic.in>
2019-04-29 16:40:41 +0530

    added logic for viewtype. Currently hard coded stuff, refactor later

--- app/src/main/java/co/chatbot/view/ChatAdapter.java
+++ app/src/main/java/co/chatbot/view/ChatAdapter.java
@@ -1,16 +1,42 @@
 package co.chatbot.view;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.List;
+
+import co.chatbot.R;
+import co.chatbot.data.models.Message;
+
 public class ChatAdapter extends RecyclerView.Adapter {
+
+    private static final int MY_MESSAGE = 1;
+    private static final int OTHERS_MESSAGE = 2;
+    private Context context;
+    List<Message> messages;
+
+    public ChatAdapter(Context context) {
+        this.context = context;
+    }
 
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+        switch (getItemViewType(i)) {
+            case MY_MESSAGE:
+                return new MyMessageViewHolder(LayoutInflater.from(context)
+                        .inflate(R.layout.chat_item_right_side, viewGroup, false));
+            case OTHERS_MESSAGE:
+                return new MyMessageViewHolder(LayoutInflater.from(context)
+                        .inflate(R.layout.chat_item_left_side, viewGroup, false));
+            default:
+                throw new IllegalArgumentException("No such view type exists");
+        }
+
     }
 
     @Override
@@ -20,9 +46,19 @@ public class ChatAdapter extends RecyclerView.Adapter {
 
     @Override
     public int getItemCount() {
-        return 0;
+        if (messages == null)
+            return 0;
+        return messages.size();
     }
 
+    @Override
+    public int getItemViewType(int position) {
+        if (messages.get(position).getSenderId().equalsIgnoreCase("ajsnsjssjf")) {
+            return MY_MESSAGE;
+        } else {
+            return OTHERS_MESSAGE;
+        }
+    }
 
     class MyMessageViewHolder extends RecyclerView.ViewHolder {
 
