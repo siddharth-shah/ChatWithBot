db273bcdcde93d882557a5c1431b1646f94ad47a
Siddharth <siddharth.shah@elanic.in>
2019-04-29 15:41:13 +0530

    moved MainActvity to view package

--- app/src/main/java/co/chatbot/view/MainActivity.java
+++ app/src/main/java/co/chatbot/view/MainActivity.java
@@ -1,7 +1,9 @@
-package co.chatbot;
+package co.chatbot.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import co.chatbot.R;
 
 public class MainActivity extends AppCompatActivity {
 
