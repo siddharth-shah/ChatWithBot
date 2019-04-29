3a73af27c9a879c379797c6440a3e7a33233ee30
Siddharth <siddharth.shah@elanic.in>
2019-04-29 15:05:47 +0530

    Figuring out data part first. Adding class to get ApiClient

---
+++ app/src/main/java/co/chatbot/data/network/ApiClient.java
@@ -0,0 +1,25 @@
+package co.chatbot.data.network;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.moshi.MoshiConverterFactory;
+
+public class ApiClient {
+    public static final String BASE_URL = "http://www.personalityforge.com/api/";
+
+
+    private static Retrofit retrofit = null;
+
+
+    public static Retrofit getClient() {
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(MoshiConverterFactory.create())
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
