023240620621f84c2b3ec49d0f1b956d5619f61f
Siddharth <siddharth.shah@elanic.in>
2019-04-30 00:06:28 +0530

    added method to get DaoSession instance to access DB

--- app/src/main/java/co/chatbot/ChatApplication.java
+++ app/src/main/java/co/chatbot/ChatApplication.java
@@ -2,6 +2,26 @@ package co.chatbot;
 
 import android.app.Application;
 
+import org.greenrobot.greendao.database.Database;
+
+import co.chatbot.data.database.DatabaseOpenHelper;
+import co.chatbot.data.database.models.DaoMaster;
+import co.chatbot.data.database.models.DaoSession;
+
 public class ChatApplication extends Application {
 
+    private DaoSession daoSession;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        DaoMaster.OpenHelper openHelper
+                = new DatabaseOpenHelper(this, "chatbot-db");
+        Database database = openHelper.getWritableDb();
+        daoSession = new DaoMaster(database).newSession();
+    }
+
+    public DaoSession getDaoSession() {
+        return daoSession;
+    }
 }
