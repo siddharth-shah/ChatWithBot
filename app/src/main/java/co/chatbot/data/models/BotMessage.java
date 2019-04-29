f4480c2b4be35cca9e890e1d578a6ba092039e42
Siddharth <siddharth.shah@elanic.in>
2019-04-29 15:20:11 +0530

    added model classes to parse bot response from server

---
+++ app/src/main/java/co/chatbot/data/models/BotMessage.java
@@ -0,0 +1,7 @@
+package co.chatbot.data.models;
+
+public class BotMessage {
+    String chatBotName;
+    int chatBotID;
+    String message;
+}
---
+++ app/src/main/java/co/chatbot/data/models/BotResponse.java
@@ -0,0 +1,8 @@
+package co.chatbot.data.models;
+
+public class BotResponse {
+
+    int success;
+    String errorMessage;
+    BotResponse message;
+}
