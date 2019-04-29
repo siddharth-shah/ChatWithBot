46238fc4558638cf90d1b1b116cff04a0c4a707e
Siddharth <siddharth.shah@elanic.in>
2019-04-29 15:22:57 +0530

    added apiCall for sending message

---
+++ app/src/main/java/co/chatbot/data/network/ChatApi.java
@@ -0,0 +1,14 @@
+package co.chatbot.data.network;
+
+import java.util.HashMap;
+
+import co.chatbot.data.models.BotResponse;
+import io.reactivex.Observable;
+import retrofit2.http.GET;
+import retrofit2.http.QueryMap;
+
+public interface ChatApi {
+
+    @GET("/chat/")
+    Observable<BotResponse> sendMessage(@QueryMap HashMap<String, String> params);
+}
