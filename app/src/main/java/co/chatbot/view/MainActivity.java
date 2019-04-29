47f7c846ffe386952bd5d160238aace0259e3bf9
Siddharth <siddharth.shah@elanic.in>
2019-04-29 16:50:06 +0530

    recyclerview setup

--- app/src/main/java/co/chatbot/view/MainActivity.java
+++ app/src/main/java/co/chatbot/view/MainActivity.java
@@ -1,21 +1,31 @@
 package co.chatbot.view;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import co.chatbot.R;
 import co.chatbot.data.models.Message;
-import co.chatbot.presenter.ChatPresenterImpl;
 
 public class MainActivity extends AppCompatActivity implements ChatView {
+
+    RecyclerView chatList;
+    ChatAdapter chatAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ChatPresenterImpl presenter =
-                new ChatPresenterImpl(this);
-        presenter.sendMessage("Hey! What's up?");
+        chatAdapter = new ChatAdapter(this);
+        setupRecyclerView();
+        chatList.setAdapter(chatAdapter);
+    }
+
+    private void setupRecyclerView() {
+        chatList = findViewById(R.id.chat_list);
+        //stack like list for chatItems
+        chatList.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, true));
     }
 
     @Override
