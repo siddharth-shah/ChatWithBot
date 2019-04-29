69cb98d92524c5ca7234bb556e143f5a4a884f5c
Siddharth <siddharth.shah@elanic.in>
2019-04-29 19:26:13 +0530

    added getters and setters

--- app/src/main/java/co/chatbot/data/models/BotMessage.java
+++ app/src/main/java/co/chatbot/data/models/BotMessage.java
@@ -4,4 +4,28 @@ public class BotMessage {
     String chatBotName;
     int chatBotID;
     String message;
+
+    public String getChatBotName() {
+        return chatBotName;
+    }
+
+    public void setChatBotName(String chatBotName) {
+        this.chatBotName = chatBotName;
+    }
+
+    public int getChatBotID() {
+        return chatBotID;
+    }
+
+    public void setChatBotID(int chatBotID) {
+        this.chatBotID = chatBotID;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
--- app/src/main/java/co/chatbot/data/models/BotResponse.java
+++ app/src/main/java/co/chatbot/data/models/BotResponse.java
@@ -5,4 +5,29 @@ public class BotResponse {
     int success;
     String errorMessage;
     BotMessage message;
+
+    public int getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(int success) {
+        this.success = success;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public BotMessage getMessage() {
+        return message;
+    }
+
+    public void setMessage(BotMessage message) {
+        this.message = message;
+    }
 }
+
