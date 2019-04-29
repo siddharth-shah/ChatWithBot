0f6fc1e468ca29401d255cddcb8365d46ecd2e83
Siddharth <siddharth.shah@elanic.in>
2019-04-29 23:40:10 +0530

    added helper class for Database

---
+++ app/src/main/java/co/chatbot/data/database/DatabaseHelper.java
@@ -0,0 +1,24 @@
+package co.chatbot.data.database;
+
+import android.content.Context;
+
+import org.greenrobot.greendao.database.Database;
+
+import co.chatbot.data.database.models.DaoMaster;
+
+public class DatabaseHelper extends DaoMaster.OpenHelper {
+
+    public DatabaseHelper(Context context, String name) {
+        super(context, name);
+    }
+
+    @Override
+    public void onCreate(Database db) {
+        super.onCreate(db);
+    }
+
+    @Override
+    public void onUpgrade(Database db, int oldVersion, int newVersion) {
+        super.onUpgrade(db, oldVersion, newVersion);
+    }
+}
