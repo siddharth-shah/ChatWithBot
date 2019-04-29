a0dcc250b4e87e088be162f72f4242621fab7a15
Siddharth <siddharth.shah@elanic.in>
2019-04-29 15:15:52 +0530

    added some appconstants

---
+++ app/src/main/java/co/chatbot/AppConstants.java
@@ -0,0 +1,14 @@
+package co.chatbot;
+
+public class AppConstants {
+
+    public static final String QUERY_PARAM_API_KEY = "apiKey";
+    public static final String QUERY_PARAM_CHAT_BOT_ID = "chatBotID";
+    public static final String QUERY_PARAM_EXTERNAL_ID = "externalID";
+    public static final String QUERY_PARAM_MESSAGE = "message";
+    public static final String QUERY_FIRST_NAME = "firstName";
+    public static final String API_KEY = "6nt5d1nJHkqbkphe";
+    public static final String CHAT_BOT_ID = "63906";
+
+
+}
