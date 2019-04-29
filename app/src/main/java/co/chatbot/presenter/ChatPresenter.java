3089aa1d333a980aab272f94f90f6b1e99ae5823
Siddharth <siddharth.shah@elanic.in>
2019-04-30 01:48:54 +0530

    addedMethod to fetch all the chats between user and bot

--- app/src/main/java/co/chatbot/presenter/ChatPresenter.java
+++ app/src/main/java/co/chatbot/presenter/ChatPresenter.java
@@ -2,4 +2,6 @@ package co.chatbot.presenter;
 
 public interface ChatPresenter {
     void sendMessage(String message, String senderId, String botId);
+    void getAllMessages(String userId, String botId);
+
 }
