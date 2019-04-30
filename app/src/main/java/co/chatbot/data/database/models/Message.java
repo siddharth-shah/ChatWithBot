1877dfce4671d85cb87877f8273cfdcb796f2446
Siddharth <siddharth.shah@elanic.in>
2019-04-30 17:21:32 +0530

    added primary key

--- app/src/main/java/co/chatbot/data/database/models/Message.java
+++ app/src/main/java/co/chatbot/data/database/models/Message.java
@@ -1,11 +1,15 @@
 package co.chatbot.data.database.models;
 
 import org.greenrobot.greendao.annotation.Entity;
-import org.greenrobot.greendao.annotation.Property;
 import org.greenrobot.greendao.annotation.Generated;
+import org.greenrobot.greendao.annotation.Id;
+import org.greenrobot.greendao.annotation.Property;
 
 @Entity(nameInDb = "message")
 public class Message {
+    @Id
+    @Property(nameInDb = "localId")
+    Long localId;
 
     @Property(nameInDb = "chatbotID")
     String chatBotID;
@@ -19,9 +23,11 @@ public class Message {
     long createdAt;
     @Property(nameInDb = "status")
     String status;
-    @Generated(hash = 145624102)
-    public Message(String chatBotID, String senderID, String externalID,
-            String messageText, long createdAt, String status) {
+
+    @Generated(hash = 1583690798)
+    public Message(Long localId, String chatBotID, String senderID,
+            String externalID, String messageText, long createdAt, String status) {
+        this.localId = localId;
         this.chatBotID = chatBotID;
         this.senderID = senderID;
         this.externalID = externalID;
@@ -29,44 +35,65 @@ public class Message {
         this.createdAt = createdAt;
         this.status = status;
     }
+
     @Generated(hash = 637306882)
     public Message() {
     }
+
     public String getChatBotID() {
         return this.chatBotID;
     }
+
     public void setChatBotID(String chatBotID) {
         this.chatBotID = chatBotID;
     }
+
     public String getSenderID() {
         return this.senderID;
     }
+
     public void setSenderID(String senderID) {
         this.senderID = senderID;
     }
+
     public String getExternalID() {
         return this.externalID;
     }
+
     public void setExternalID(String externalID) {
         this.externalID = externalID;
     }
+
     public String getMessageText() {
         return this.messageText;
     }
+
     public void setMessageText(String messageText) {
         this.messageText = messageText;
     }
+
     public long getCreatedAt() {
         return this.createdAt;
     }
+
     public void setCreatedAt(long createdAt) {
         this.createdAt = createdAt;
     }
+
     public String getStatus() {
         return this.status;
     }
+
     public void setStatus(String status) {
         this.status = status;
+    }
+
+    public Long getLocalId() {
+        return this.localId;
+    }
+
+    public void setLocalId(Long localId) {
+        this.localId = localId;
     }
 
 
